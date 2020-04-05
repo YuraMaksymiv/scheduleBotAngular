0ef3eef1d6f483856d2c22356b686565099f468c
yura <yu.maksymiv@gmail.com>
2020-04-05 12:20:36 +0300

    innitial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {APIResponse} from './Interfaces/API_Response';
+import {GroupsService} from './services/groups.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'scheduleBotAngular';
+export class AppComponent implements OnInit {
+  constructor(
+    private router: Router
+  ) {}
+
+  isLogged: boolean;
+
+  checkLogin() {
+    if(localStorage.getItem("token")) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+      this.router.navigate(['/login'])
+    }
+  }
+
+  ngOnInit() {
+    this.checkLogin()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupsComponent } from './groups/groups.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule, MatListModule, MatPaginatorModule, MatSelectModule, MatTableModule} from '@angular/material';
+import {MatCardModule} from '@angular/material';
+import { GroupListsComponent } from './groups/group-lists/group-lists.component';
+import { GroupNamesComponent } from './groups/group-names/group-names.component';
+import { UsersForGroupComponent } from './groups/users-for-group/users-for-group.component';
+import {MatRippleModule} from '@angular/material';
+import { ScheduleComponent } from './schedule/schedule.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ScheduleItemComponent } from './schedule/schedule-item/schedule-item.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  // {path: '', component: AppComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'sections', component: GroupsComponent},
+  {path: 'sections/:section', component: GroupListsComponent, pathMatch: 'full'},
+  {path: 'sections/:section/groups/:groupName', component: GroupNamesComponent, pathMatch: 'full'},
+  {path: 'sections/:section/groups/:groupName/course/:courseName', component: UsersForGroupComponent, pathMatch: 'full'},
+  {path: 'schedule', component: ScheduleComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupsComponent,
+    GroupListsComponent,
+    GroupNamesComponent,
+    UsersForGroupComponent,
+    ScheduleComponent,
+    ScheduleItemComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    RouterModule.forRoot(routes),
+    MatRippleModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    FormsModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
