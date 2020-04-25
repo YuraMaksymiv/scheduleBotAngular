36c848fe7eea612bf39a12f2f3417b1dbdc5d599
yura <yu.maksymiv@gmail.com>
2020-04-25 19:16:14 +0300

    changes

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+import {GroupsService} from './services/groups.service';
 
 
 @Injectable()
 export class ParamInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req);
+  constructor(private Groups: GroupsService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap(
+        (event: HttpEvent<any>) => {
+        }, (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              console.log('%c UNAUTHORIZED /!\\ ', 'border: 1px red solid; color: red;');
+              localStorage.removeItem('token');
+            }
+          }
+        }
+      )
+    );
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {APIResponse} from './Interfaces/API_Response';
 import {GroupsService} from './services/groups.service';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {Location} from '@angular/common';
+import {Observable} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +14,26 @@ import {Location} from '@angular/common';
 export class AppComponent implements OnInit {
   constructor(
     private router: Router,
-    private _location: Location
-  ) {}
+    private _location: Location,
+
+  ) {
+    this.navigationEnd = router.events.pipe(
+      filter(evt => evt instanceof NavigationEnd)
+    ) as Observable<NavigationEnd>;
+  }
+
+  navigationEnd: Observable<NavigationEnd>;
 
   isLogged: boolean;
   inMenu = false;
 
-  checkLogin() {
-    if(localStorage.getItem("token")) {
-      this.isLogged = true;
-    } else {
-      this.isLogged = false;
-      // this.router.navigate(['/login']);
-    }
-  }
+  // checkLogin() {
+  //   if(localStorage.getItem("token")) {
+  //     this.isLogged = true;
+  //   } else {
+  //     this.isLogged = false;
+  //   }
+  // }
 
   backClick(): void {
     this._location.back();
@@ -38,7 +46,13 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.checkLogin()
+    this.navigationEnd.subscribe(evt => {
+      if (!this.router.url.includes('login')){
+        this.isLogged = true;
+      } else {
+        this.isLogged = false;
+      }
+    })
   }
 
 }
